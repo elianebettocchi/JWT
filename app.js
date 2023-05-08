254342d054fe6a6c75e18616de8b6aa0206ba955
Eliane <elianebettocchi@gmail.com>
2023-05-08 18:09:12 -0400

    push

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
-
+app.use(express.json())
 // view engine
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://Lmiller007:OreoCookies11@jwt-lesson.wcoav.mongodb.net/JWT-Lesson?retryWrites=true&w=majority';
+const dbURI = 'mongodb+srv://elianebettocchi:0ss1b4l3f3w4@cluster0.mqhjhgp.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
@@ -18,3 +19,4 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes);
---
+++ models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLenght: 6
+    }
+})
+const User = mongoose.model('user', userSchema);
+module.exports = User;
