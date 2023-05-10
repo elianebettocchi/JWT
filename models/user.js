3b74cdd3321f39042c0fb5285baa76d21b78bcd0
Eliane Bettocchi <elianebettocchi@gmail.com>
2023-05-09 20:24:34 -0400

    Add files via upload

--- models/user.js
+++ models/user.js
@@ -1,16 +1,29 @@
 const mongoose = require('mongoose');
+const {isEmail} = require('validator');
+const bcrypt = require ('bcrypt')
 const userSchema = new mongoose.Schema({
     email:{
         type: String,
-        required: true,
+        required: [true, 'Please enter an email'],
         unique: true,
-        lowercase: true
+        lowercase: true,
+        validate: [isEmail, 'Please enter a valid email'],
     },
     password: {
         type: String,
-        required: true,
-        minLenght: 6
+        required: [true, 'Please enter a password'],
+        minLenght: [6, 'Minimum lenght is 6 characters']
     }
 })
+userSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt)
+    next();
+})
+userSchema.post('save', function(doc, next){
+    console.log('new user was created and saved', doc);
+    next();
+})
 const User = mongoose.model('user', userSchema);
+
 module.exports = User;
